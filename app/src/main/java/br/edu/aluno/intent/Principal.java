b9f906a63f2f1aa396ede32fb70bece52dbcb062
camelogamerdf <camelocml@gmail.com>
2017-03-24 20:51:18 -0300

    criação de um botão na tela principal

--- app/src/main/java/br/edu/aluno/intent/Principal.java
+++ app/src/main/java/br/edu/aluno/intent/Principal.java
@@ -2,8 +2,13 @@ package br.edu.aluno.intent;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
+import static android.R.attr.button;
 
 public class Principal extends AppCompatActivity {
+
+    private Button botaoum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
